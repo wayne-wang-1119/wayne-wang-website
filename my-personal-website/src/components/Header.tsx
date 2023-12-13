ae7b140f774e49df526b087c93dfb60cf348c805
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2023-12-13 00:10:24 -0800

    initial setup

---
+++ my-personal-website/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <nav>{/* Navigation items here */}</nav>
+  </header>
+);
+
+export default Header;
---
+++ my-personal-website/src/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContent = () => (
+  <main>
+    <section>
+      {/* Main content such as a brief introduction or featured work */}
+    </section>
+  </main>
+);
+
+export default MainContent;
