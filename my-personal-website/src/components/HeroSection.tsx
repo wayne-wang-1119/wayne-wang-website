1967f8c92fc5f6e735ba054664fd308cf0fd6b43
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2023-12-13 01:14:42 -0800

    ok

--- my-personal-website/src/components/HeroSection.tsx
+++ my-personal-website/src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import "./HeroSection.css"; // Make sure to create this CSS file
 const HeroSection = () => (
   <div className="hero-container">
     <div className="hero-text">
-      <h1 className="hero-title">Wayne Wang</h1>
+      <h1 className="hero-title">Wayne Wang,</h1>
       <p className="hero-subtitle">
         <span className="label software-engineer">Software Engineer</span>
         ,&nbsp;
