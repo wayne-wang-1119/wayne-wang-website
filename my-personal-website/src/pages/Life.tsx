fa22d0918b06545e472d77ddc500505a15db57d6
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2023-12-13 02:46:58 -0800

    yea why not

--- my-personal-website/src/pages/Life.tsx
+++ my-personal-website/src/pages/Life.tsx
@@ -1,14 +1,28 @@
+// src/pages/AboutMe.tsx
 import React from "react";
+import "./Life.css"; // Make sure to have an accompanying CSS file
 
-const Life = () => (
-  <div>
+const AboutMe = () => (
+  <div className="about-me-container">
     <h1>About Me</h1>
     <p>
-      I'm Wayne Wang, a software engineer and AI researcher with a strong
-      educational background from UC Berkeley. Proficient in a wide range of
-      programming languages, software, and frameworks.
+      Hello! I'm Wayne Wang, an AI researcher and software engineer with a
+      passion for developing innovative solutions.
+    </p>
+    <p>
+      In my professional journey, I've had the privilege of working with
+      cutting-edge technologies at Berkeley EECS and co-founding GotHub.AI,
+      where I manage a significant AI project.
+    </p>
+    <p>
+      I specialize in reinforcement learning, cloud computing, and full-stack
+      development. Outside of work, I'm an avid reader and tech enthusiast.
+    </p>
+    <p>
+      I am always looking for new challenges and opportunities to grow and make
+      a meaningful impact in the field of technology.
     </p>
   </div>
 );
 
-export default Life;
+export default AboutMe;
