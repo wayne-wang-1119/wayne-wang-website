b92a7d8624a25bbf33d01afb6abd607c7a4315ca
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2024-02-21 15:22:36 -0800

    update HeroSection.tsx and output.css

--- my-personal-website/src/components/HeroSection.tsx
+++ my-personal-website/src/components/HeroSection.tsx
@@ -1,18 +1,44 @@
-// src/components/HeroSection.tsx
-import React from "react";
-import "./HeroSection.css"; // Make sure to create this CSS file
+import React, { useEffect, useState } from "react";
 
-const HeroSection = () => (
-  <div className="hero-container">
-    <div className="hero-text">
-      <h1 className="hero-title">Wayne Wang,</h1>
-      <p className="hero-subtitle">
-        <span className="label software-engineer">Software Engineer</span>
-        ,&nbsp;
-        <span className="label ai-researcher">AI Researcher</span>
-      </p>
+const HeroSection = () => {
+  // State to control the fade-in effect
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    // Trigger the fade-in effect once the component mounts
+    setFadeIn(true);
+  }, []);
+
+  return (
+    <div className="relative flex flex-col justify-center items-center h-screen bg-gray-700 text-white text-center p-12 overflow-hidden">
+      {/* Content */}
+      <div
+        className={`transition-opacity duration-1000 ${
+          fadeIn ? "opacity-100" : "opacity-0"
+        } translate-y-0`}
+      >
+        <h1 className="text-6xl font-bold leading-tight mb-4 text-gray-300">
+          This is <span className="text-gray-200">Wayne Wang,</span>
+        </h1>
+        <p className="text-xl font-light">
+          Your utility guy in{" "}
+          <span className="text-gray-200 font-medium">
+            &lt;Fullstack Web Development /&gt;
+          </span>{" "}
+          ,
+          <span className="text-gray-200 font-medium">
+            &lt;LLM Engineering /&gt;
+          </span>{" "}
+          and
+          <span className="text-gray-200 font-medium">
+            &lt;AI Research /&gt;
+          </span>{" "}
+          ,
+        </p>
+        <p className="text-gray-500">and a part time car mechanic.</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default HeroSection;
