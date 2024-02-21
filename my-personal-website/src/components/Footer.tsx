ac765658dd46d10255eacaed7d151861d7359820
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2024-02-21 15:02:01 -0800

    header

--- my-personal-website/src/components/Footer.tsx
+++ my-personal-website/src/components/Footer.tsx
@@ -5,40 +5,45 @@ import {
   faGithub,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-      <ul>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/wayne-wang-380029197/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/wayne-wang-1119"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} /> GitHub
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/thehaohanwang"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faTwitter} /> Twitter
-          </a>
-        </li>
-      </ul>
+    <footer className="bg-[#c4820e] py-2.5 text-center">
+      <nav className="container mx-auto">
+        <ul className="list-none p-0 flex justify-center items-center">
+          <li className="inline-block mx-2.5">
+            <Link
+              to="https://www.linkedin.com/in/wayne-wang-380029197/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="no-underline text-[#003262] hover:text-[#3b7ea1]"
+            >
+              <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+            </Link>
+          </li>
+          <li className="inline-block mx-2.5">
+            <Link
+              to="https://github.com/wayne-wang-1119"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="no-underline text-[#003262] hover:text-[#3b7ea1]"
+            >
+              <FontAwesomeIcon icon={faGithub} /> GitHub
+            </Link>
+          </li>
+          <li className="inline-block mx-2.5">
+            <Link
+              to="https://twitter.com/thehaohanwang"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="no-underline text-[#003262] hover:text-[#3b7ea1]"
+            >
+              <FontAwesomeIcon icon={faTwitter} /> Twitter
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 };
